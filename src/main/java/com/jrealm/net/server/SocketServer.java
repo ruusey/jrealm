7cbad5127368cd80a73be30d99b51a1f5b4d26d1
Robert Usey <ruusey@gmail.com>
2023-10-16 10:55:26 -0400

    Accept new client connections and process client packets in separate threads

--- src/main/java/com/jrealm/net/server/SocketServer.java
+++ src/main/java/com/jrealm/net/server/SocketServer.java
@@ -11,6 +11,7 @@ import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import com.jrealm.game.util.WorkerThread;
 import com.jrealm.net.BlankPacket;
 import com.jrealm.net.Packet;
 import com.jrealm.net.client.SocketClient;
@@ -22,14 +23,15 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 public class SocketServer extends Thread {
 	public static final String LOCALHOST = "127.0.0.1";
-	
+
 	private static final int BUFFER_CAPACITY = 65536 * 10;
 
 	private ServerSocket serverSocket;
-	private boolean shutdown = false;
+	private boolean shutdownSocketAccept = false;
+	private boolean shutdownProcessing = false;
 	private byte[] localBuffer = new byte[SocketServer.BUFFER_CAPACITY];
 	private int localBufferIndex = 0;
 	private byte[] remoteBuffer = new byte[SocketServer.BUFFER_CAPACITY];
@@ -43,7 +45,6 @@ public class SocketServer extends Thread {
 
 	private Map<String, Socket> clients = new ConcurrentHashMap<>();
 
-
 	public SocketServer(int port) {
 		SocketServer.log.info("Creating local server at port {}", port);
 		try {
@@ -55,21 +56,45 @@ public class SocketServer extends Thread {
 
 	@Override
 	public void run() {
-		try {
-			Socket socket = this.serverSocket.accept();
-			String remoteAddr = socket.getInetAddress().getHostAddress();
-			this.clients.put(remoteAddr, socket);
-			TextPacket welcomeMessage = TextPacket.create("SYSTEM", "Ruusey", "Welcome to JRealm!");
-			welcomeMessage.serializeWrite(new DataOutputStream(socket.getOutputStream()));
-			log.info("Server accepted new connection from Remote Address {}",remoteAddr);
-		}catch(Exception e) {
-			log.error("Failed to accept incoming socket connection, exiting...",e);
-			return;
-		}
 
-		while (!this.shutdown) {
+		Runnable socketAccept = () -> {
+			log.info("Server now accepting inbound connections...");
+			while (!this.shutdownSocketAccept) {
+				try {
+					Socket socket = this.serverSocket.accept();
+					String remoteAddr = socket.getInetAddress().getHostAddress();
+					log.info("Accepted new socket connection from {}", remoteAddr);
+					this.clients.put(remoteAddr, socket);
+					TextPacket welcomeMessage = TextPacket.create("SYSTEM", "Ruusey", "Welcome to JRealm!");
+					welcomeMessage.serializeWrite(new DataOutputStream(socket.getOutputStream()));
+					log.info("Server accepted new connection from Remote Address {}", remoteAddr);
+				} catch (Exception e) {
+					log.error("Failed to accept incoming socket connection, exiting...", e);
+					// return;
+				}
+			}
+		};
+
+		Runnable processClients = () -> {
+			log.info("Begin processing client packets...");
+			while (!this.shutdownProcessing) {
+				try {
+					this.enqueueClientPackets();
+					Thread.sleep(100);
+				} catch (Exception e) {
+					SocketServer.log.error("Failed to parse client input {}", e.getMessage());
+				}
+			}
+		};
+
+		WorkerThread.submitAndRun(socketAccept, processClients);
+
+	}
+
+	private void enqueueClientPackets() {
+		for (Map.Entry<String, Socket> client : this.clients.entrySet()) {
 			try {
-				InputStream stream = this.clients.get(LOCALHOST).getInputStream();
+				InputStream stream = client.getValue().getInputStream();
 				int bytesRead = stream.read(this.remoteBuffer, this.remoteBufferIndex,
 						this.remoteBuffer.length - this.remoteBufferIndex);
 				if (bytesRead == -1)
@@ -95,7 +120,7 @@ public class SocketServer extends Thread {
 						this.packetQueue.add(new BlankPacket(packetId, packetBytes));
 					}
 				}
-			}catch(Exception e) {
+			} catch (Exception e) {
 				SocketServer.log.error("Failed to parse client input {}", e.getMessage());
 			}
 		}
