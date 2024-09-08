618bfe493ed625810abb422c887229f8005b6624
Robert Usey <ruusey@gmail.com>
2024-09-08 17:38:56 -0400

    Update run-jrealm.sh

--- run-jrealm.sh
+++ run-jrealm.sh
@@ -1,1 +1,1 @@
-java -jar ./target/jrealm.jar -client 18.117.127.158 ru@jrealm.com password 7304c310-e36a-4252-8c5a-3b3893856106
+java -jar ./target/jrealm.jar -client {DATA_SERVER_IP} {ACCOUNT_EMAIL} {ACCOUNT_PASSWORD} {CHARRACTER_UUID}
