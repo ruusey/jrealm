e364cd0a0d91fe27ce9d3edba94fb7bb3257c659
Robert Usey <ruusey@gmail.com>
2025-03-03 08:15:50 -0500

    Update run-jrealm.sh

--- run-jrealm.sh
+++ run-jrealm.sh
@@ -1,7 +1,12 @@
+#IN TERMINAL
 curl -s "https://get.sdkman.io" | bash
+
+#Restart Terminal after this step^
 sdk list java 
 sdk install java 17.0.4.1-tem
 
+java -version
+#Should print 17.X.X.X^
 java -jar ./jrealm.jar
 
 
