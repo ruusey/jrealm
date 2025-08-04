2df62277520f00480e35cde975052c4882437ff8
Robert Usey <ruusey@gmail.com>
2025-08-03 23:43:16 -0400

    Bump version

--- run-jrealm.bat
+++ run-jrealm.bat
@@ -1,1 +1,4 @@
-java -jar ./target/jrealm.jar -client 18.117.127.158 {EMAIL} {PASSWORD} {CHARACTER-UUID}
+start java -jar ../jrealm-data/target/jrealm-data.jar
+timeout 8 > NUL
+start java -jar ./target/jrealm.jar -embedded 127.0.0.1 ru@jrealm.com password 146cdcbd-4266-4148-baef-4381eb22f4ad
+pause
