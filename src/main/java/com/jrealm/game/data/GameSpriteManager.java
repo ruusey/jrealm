37fef7ce82b9d6ebd473cbcafe86b6517efa9741
Robert Usey <ruusey@gmail.com>
2026-04-01 19:54:51 -0400

    Load right sprites

--- src/main/java/com/jrealm/game/data/GameSpriteManager.java
+++ src/main/java/com/jrealm/game/data/GameSpriteManager.java
@@ -36,7 +36,7 @@ public class GameSpriteManager {
             "rotmg-tiles.png", "rotmg-tiles-1.png", "rotmg-tiles-2.png", "rotmg-tiles-all.png",
             "rotmg-abilities.png", "rotmg-misc.png",
             "rotmg-classes-0.png", "rotmg-classes-1.png", "rotmg-classes-2.png", "rotmg-classes-3.png",
-            "lofi_char.png", "lofi_environment.png", "lofi_obj.png",
+            "lofi_char.png", "lofi_environment.png", "lofi_obj.png", "lofiObj3.png",
             "chars8x8rBeach.png", "chars8x8rHero2.png", "cursedLibraryChars16x16.png",
             "d3Chars8x8r.png", "cursedLibraryChars8x8.png", "cursedLibraryObjects8x8.png",
             "archbishopObjects16x16.png", "autumnNexusObjects16x16.png",
