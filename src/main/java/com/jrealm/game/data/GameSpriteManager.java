91ceda8df7717b4913ce380ee8d897eb0d0e8882
Robert Usey <ruusey@gmail.com>
2026-04-03 10:53:02 -0400

    Add prot tome

--- src/main/java/com/jrealm/game/data/GameSpriteManager.java
+++ src/main/java/com/jrealm/game/data/GameSpriteManager.java
@@ -36,7 +36,7 @@ public class GameSpriteManager {
             "rotmg-tiles.png", "rotmg-tiles-1.png", "rotmg-tiles-2.png", "rotmg-tiles-all.png",
             "rotmg-abilities.png", "rotmg-misc.png",
             "rotmg-classes-0.png", "rotmg-classes-1.png", "rotmg-classes-2.png", "rotmg-classes-3.png",
-            "lofi_char.png", "lofi_environment.png", "lofi_obj.png", "lofiObj3.png", "lofiObjBig.png",
+            "lofi_char.png", "lofi_environment.png", "lofi_obj.png", "lofiObj2.png", "lofiObj3.png", "lofiObjBig.png",
             "lofiEnvironment.png", "lofiEnvironment2.png", "lofiEnvironment3.png",
             "lofi_dungeon_features.png",
             "chars8x8rBeach.png", "chars8x8rHero2.png", "cursedLibraryChars16x16.png",
