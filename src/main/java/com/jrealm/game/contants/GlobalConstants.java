ef0de0558893ffd7bcc6c2b867eefc057abb3dd6
Robert Usey <ruusey@gmail.com>
2026-04-06 14:11:38 -0400

    Reduce player size back to 32px

--- src/main/java/com/jrealm/game/contants/GlobalConstants.java
+++ src/main/java/com/jrealm/game/contants/GlobalConstants.java
@@ -18,7 +18,7 @@ public class GlobalConstants {
 
     public static final int PLAYER_SIZE = 28;
 
-    public static final int PLAYER_RENDER_SIZE = 38;
+    public static final int PLAYER_RENDER_SIZE = 32;
     
     public static final long SOCKET_READ_TIMEOUT = 15000;
 }
